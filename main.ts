d614663c86f426b6396b1a8c0452de1dec154ba8
pakawat2549 <75378790+pakawat2549@users.noreply.github.com>
2020-12-03 19:54:29 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LEDswitch.turnOn, maqueen.LED.LEDLeft)
+    maqueen.writeLED(maqueen.LEDswitch.turnOff, maqueen.LED.LEDRight)
+    music.playTone(392, music.beat(BeatFraction.Half))
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LEDswitch.turnOff, maqueen.LED.LEDLeft)
+    maqueen.writeLED(maqueen.LEDswitch.turnOn, maqueen.LED.LEDRight)
+    music.playTone(330, music.beat(BeatFraction.Half))
+    basic.pause(100)
+})
