efb9c5d0d6f8ef42d1b109f4f6bab65ceeac2739
pakawat2549 <75378790+pakawat2549@users.noreply.github.com>
2020-12-05 13:58:36 +0700

    siren2

--- main.ts
+++ main.ts
@@ -8,3 +8,50 @@ basic.forever(function () {
     music.playTone(330, music.beat(BeatFraction.Half))
     basic.pause(100)
 })
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # . # # .
+        # . . # .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # # # # .
+        # . . # .
+        # . . # .
+        # . . # .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # . # . .
+        # . # . .
+        # . # . .
+        # # . . .
+        `)
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P13) == 0 && pins.digitalReadPin(DigitalPin.P14) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    } else if (pins.digitalReadPin(DigitalPin.P13) == 1 && pins.digitalReadPin(DigitalPin.P14) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+    } else if (pins.digitalReadPin(DigitalPin.P13) == 0 && pins.digitalReadPin(DigitalPin.P14) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    }
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 15) {
+        while (maqueen.Ultrasonic(PingUnit.Centimeters) < 20) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        }
+    }
+})
